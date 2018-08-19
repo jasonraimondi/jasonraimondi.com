82ddecbf50fe4af050131e8ce6b877b00a8d9f0e
Jason Raimondi <jason@raimondi.us>
2018-08-18 23:16:35 -0700

    Sidebar working, content area working ish.

--- assets/src/main.ts
+++ assets/src/main.ts
@@ -5,7 +5,6 @@ import ImageGallery from './components/ImageGallery.vue';
 import ImagePop from './components/ImagePop.vue';
 import VideoContainer from './components/VideoContainer.vue';
 
-
 const vm = new Vue({
   el: '#post-content',
   components: {
@@ -14,3 +13,26 @@ const vm = new Vue({
     VideoContainer,
   },
 });
+
+let isSidebarOpen = false;
+
+// Toggle button
+document.querySelector('.toggle-button').addEventListener('click', function () {
+  if (isSidebarOpen) {
+    closeSidebar();
+  } else {
+    openSidebar();
+  }
+  isSidebarOpen = !isSidebarOpen;
+});
+
+function openSidebar() {
+  var element = document.querySelector('html');
+  element.classList.add('sidebar-is-open');
+}
+
+
+function closeSidebar() {
+  var element = document.querySelector('html');
+  element.classList.remove('sidebar-is-open');
+}
