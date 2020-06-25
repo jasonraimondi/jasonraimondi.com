c32ea609ce1043d4b6b05df2cbbc0438bbc96ed1
Jason Raimondi <jason@raimondi.us>
2020-06-24 21:26:12 -0700

    Refactor var names for consistency

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -42,13 +42,13 @@ const updateCreditWithLink = function () {
 };
 
 const sendExternalLinksToNewTab = function () {
-    const postDoc = document.getElementById('post-content');
-    const links = postDoc.getElementsByTagName('a');
+    const $postDoc = document.getElementById('post-content');
+    const $links = $postDoc.getElementsByTagName('a');
 
-    for (let i = 0, linksLength = links.length; i < linksLength; i++) {
-        if (links[i].hostname != window.location.hostname) {
-            links[i].target = '_blank';
-            links[i].rel = 'nofollow';
+    for (let i = 0, linksLength = $links.length; i < linksLength; i++) {
+        if ($links[i].hostname !== window.location.hostname) {
+            $links[i].target = '_blank';
+            $links[i].rel = 'nofollow';
         }
     }
 };
