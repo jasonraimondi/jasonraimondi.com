6c4bbe06455fa7b6bea99586e122f543a9b1d49f
Jason Raimondi <jason@raimondi.us>
2021-03-08 23:26:13 -0800

    refactor(wip): tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,6 @@
-const cssPresetEnv = require('postcss-preset-env');
 module.exports = {
-    plugins: [
-        cssPresetEnv({
-            stage: 1,
-            features: {
-                'color-mod-function': {
-                    unresolved: 'warn'
-                },
-                'custom-properties': {
-                    preserve: false
-                },
-                'nesting-rules': true
-            }
-        }),
-    ]
-};
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './content/**/*.html',
+    './layouts/**/*.html',
+    './taxonomy/**/*.html',
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
