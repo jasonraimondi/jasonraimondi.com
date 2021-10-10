2b1d927137267e849db366401f860982ecc14bee
Jason Raimondi <jason@raimondi.us>
2021-10-10 15:56:48 -0700

    fix: single page - anchors and top margin

--- assets/js/post.ts
+++ assets/js/post.ts
@@ -1,14 +1,11 @@
 function addHeaderDeepLinks() {
-    const postDoc = document.getElementById('post-content');
-
-    if (!postDoc) return;
-
+    const $div = document.getElementById('js-single');
     const elements = [
-        postDoc.getElementsByTagName('h2'),
-        postDoc.getElementsByTagName('h3'),
-        postDoc.getElementsByTagName('h4'),
-        postDoc.getElementsByTagName('h5'),
-        postDoc.getElementsByTagName('h6'),
+        $div.getElementsByTagName('h2'),
+        $div.getElementsByTagName('h3'),
+        $div.getElementsByTagName('h4'),
+        $div.getElementsByTagName('h5'),
+        $div.getElementsByTagName('h6'),
     ];
     for (let i = 0; i < elements.length; i++) {
         for (let k = 0; k < elements[i].length; k++) {
@@ -22,46 +19,22 @@ function addHeaderDeepLinks() {
     }
 }
 
-function updateCreditWithLink() {
-    const credit = document.getElementById('js-feature-image-credit');
+function sanitizeLinks() {
+    const $div = document.getElementById('js-single');
 
-    if (!credit) return;
+    if (!$div) return;
 
-    const [username, url] = credit.innerText.split(' ');
-
-    const $username = document.createElement('a');
-    $username.href = 'https://unsplash.com/' + username;
-    $username.innerHTML = username;
-    $username.target = '_blank';
-
-    const $url = document.createElement('a');
-    $url.href = url;
-    $url.innerHTML = url;
-    $url.target = '_blank';
-
-    credit.innerHTML = '';
-    credit.insertBefore($url, credit.firstChild);
-    credit.insertBefore(document.createTextNode(' '), credit.firstChild);
-    credit.insertBefore($username, credit.firstChild);
-}
-
-function sendExternalLinksToNewTab() {
-    const postDoc = document.getElementById('post-content');
-
-    if (!postDoc) return;
-
-    const $links = postDoc.getElementsByTagName('a');
+    const $links = $div.getElementsByTagName('a');
 
     for (let i = 0, linksLength = $links.length; i < linksLength; i++) {
         if ($links[i].hostname !== window.location.hostname) {
             $links[i].target = '_blank';
-            $links[i].rel = 'nofollow';
+            $links[i].rel = 'noopener';
         }
     }
 }
 
 void (function () {
     addHeaderDeepLinks();
-    updateCreditWithLink();
-    sendExternalLinksToNewTab();
+    sanitizeLinks();
 })();
