51d9247f855d9ece7a09e7b91a161436e43e6b1a
Jason Raimondi <jason@raimondi.us>
2023-01-28 00:40:47 -0500

    feat: add postcss-custom-media-generator package, bump packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,29 +1,27 @@
-const { resolve } = require('path');
-const { generateMediaQueries } = require('@jmondi/mobile-first');
+const {resolve} = require("path");
 
 module.exports = {
-    plugins: {
-        'postcss-import': {
-            path: [resolve(__dirname, 'assets')],
-        },
-        'postcss-mixins': {},
-        'postcss-nested': {},
-        'postcss-custom-media': {
-            importFrom: [{
-                customMedia: {
-                    '--light': '(prefers-color-scheme: light)',
-                    '--dark': '(prefers-color-scheme: dark)',
-                    ...generateMediaQueries({
-                        xsmall: 420,
-                        small: 640,
-                        medium: 768,
-                        large: 1024,
-                        xlarge: 1280,
-                        xxlarge: 1536,
-                    }),
-                },
-            }],
-        },
-        autoprefixer: {},
+  plugins: {
+    "postcss-import": {
+      path: [resolve(__dirname, "assets")],
     },
+    "postcss-mixins": {},
+    "postcss-nested": {},
+    "postcss-custom-media-generator": {
+      "--light": "prefers-color-scheme: light",
+      "--dark": "prefers-color-scheme: dark",
+      xsmall: 420,
+      small: 640,
+      medium: 768,
+      large: 1024,
+      xlarge: 1280,
+      xxlarge: 1536,
+    },
+    "postcss-preset-env": {
+      stage: 2,
+      features: {
+        "nesting-rules": true,
+      },
+    },
+  },
 };
