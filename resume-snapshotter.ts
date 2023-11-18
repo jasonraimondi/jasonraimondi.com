91281de48b335a04804f4e47b7ca8abb20a7e48e
Jason Raimondi <jason@raimondi.us>
2023-11-17 21:34:01 -0500

    fix: un-ignore static resume files

--- resume-snapshotter.ts
+++ resume-snapshotter.ts
@@ -1,16 +1,19 @@
-import { chromium } from 'playwright';
+import { chromium } from "playwright";
 
 (async () => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
   const resumePath = "./static/resume";
   try {
-    await page.goto('http://localhost:1313/resume');
+    await page.goto("http://localhost:1313/resume");
   } catch {
-    await page.goto('https://jasonraimondi.com/resume');
+    await page.goto("https://jasonraimondi.com/resume");
   }
-  await page.pdf({ path: `${resumePath}.pdf`, margin: { top: 25, bottom: 25, left: 50, right: 50 } })
-  await page.screenshot({ path: `${resumePath}.png`, fullPage: true  });
-  await page.screenshot({ path: `${resumePath}.jpg`, fullPage: true  });
+  await page.pdf({
+    path: `${resumePath}.pdf`,
+    margin: { top: 25, bottom: 25, left: 50, right: 50 },
+  });
+  // await page.screenshot({ path: `${resumePath}.png`, fullPage: true  });
+  // await page.screenshot({ path: `${resumePath}.jpg`, fullPage: true  });
   await browser.close();
 })();
